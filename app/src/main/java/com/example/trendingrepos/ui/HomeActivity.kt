3108c178ef5bfb87c5cecf26bd3c80e3ad76a81b
Mohamed ELtanani <meltanan@uncc.edu>
2022-07-27 13:02:29 -0400

    testing push

--- app/src/main/java/com/example/trendingrepos/ui/HomeActivity.kt
+++ app/src/main/java/com/example/trendingrepos/ui/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.example.trendingrepos
+package com.example.trendingrepos.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trendingrepos.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
